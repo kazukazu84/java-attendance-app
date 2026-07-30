7b0fa285ba3454dfd33b29e1a823c911fa93755f
3031027 <3031027@30310-27>
2026-07-30 13:22:34 +0900

    インポート修正

--- src/test/java/com/example/adminshift/controller/ShiftApplicationEventControllerTest.java
+++ src/test/java/com/example/adminshift/controller/ShiftApplicationEventControllerTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.security.test.context.support.WithMockUser;
