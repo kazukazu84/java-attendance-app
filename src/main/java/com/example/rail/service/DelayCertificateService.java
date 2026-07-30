af28e3d8f723315bae53eae6dbdc97436de5292b
3031027 <3031027@30310-27>
2026-07-30 11:21:29 +0900

    7/30運行状況使わなくなった機能の削除

--- src/main/java/com/example/rail/service/DelayCertificateService.java
+++
@@ -1,8 +0,0 @@
-package com.example.rail.service;
-
-import com.example.rail.dto.CertificateDto;
-import com.example.rail.dto.DelayInfoDto;
-
-public interface DelayCertificateService {
-    CertificateDto generateCertificate(DelayInfoDto delayInfo, String userId);
-}
