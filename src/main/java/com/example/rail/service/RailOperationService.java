e86b99ef95155f95cd2855b649cd26cbc9fa3518
3031027 <3031027@30310-27>
2026-07-30 15:05:53 +0900

    7/30色々処理

--- src/main/java/com/example/rail/service/RailOperationService.java
+++ src/main/java/com/example/rail/service/RailOperationService.java
@@ -1,9 +1,7 @@
 package com.example.rail.service;
 
-import com.example.rail.dto.DelayInfoDto;
 import com.example.rail.dto.RailStatusDto;
 
 public interface RailOperationService {
     RailStatusDto getStatus(String lineName);
-    DelayInfoDto getDelayInfo(String lineName);
 }
